f36a4a0a480571429766141b65bf989c723e2820
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2021-12-03 13:44:14 -0500

    Add verbose persistent flag

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,12 @@ var rootCmd = &cobra.Command{
 	Use:   "advent2021",
 	Short: "Advent of code 2021 in Go",
 }
+var Verbose bool
 
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Display more messages")
 }
