25fe108b07e40acae3d329d7e89030a44d280f54
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2021-12-03 15:06:18 -0500

    Add days module with utility methods

---
+++ days/utils.go
@@ -0,0 +1,57 @@
+package days
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func Run(day string, verbose bool) {
+	switch day {
+	case "1":
+		day1(verbose)
+	}
+}
+
+func ArrayToInteger(vs []string) []int {
+	vsm := make([]int, len(vs)-1)
+	for i, v := range vs {
+		if v != "" {
+			vsm[i] = ToInteger(v)
+		}
+	}
+	return vsm
+}
+
+func ReadLines(path string) []string {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return strings.Split(string(content), "\n")
+}
+
+func ToInteger(str string) int {
+	var value int
+	var err error
+
+	value, err = strconv.Atoi(str)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return value
+}
+
+func PrintTitle(str string) {
+	fmt.Println(str)
+	fmt.Println("=============")
+	fmt.Println()
+}
+
+func PrintSubtitle(str string) {
+	fmt.Println(str)
+	fmt.Println("-------------")
+}
