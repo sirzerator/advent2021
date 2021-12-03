bd5167f7b010d51265831f0f5306b1beea7918e4
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2021-12-03 13:21:50 -0500

    Ignore built file

--- cmd/root.go
+++ cmd/root.go
@@ -9,11 +9,6 @@ var rootCmd = &cobra.Command{
 	Short: "Advent of code 2021 in Go",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
-}
-
-func init() {
 }
